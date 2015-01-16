ad6a67ac5260d27715e40853bf460a23da9ed1b5
Brian Donovan <donovan@squareup.com>
2015-01-15 22:02:33 -0800

    Fix shorthand this which is part of another member expression.

--- test/automatic_test.js
+++ test/automatic_test.js
@@ -178,6 +178,18 @@ describe('automatic conversions', function() {
     it('does not add a dot to the shorthand prototype operator', function() {
       check('@::a', 'this::a');
     });
+
+    it('does not double-expand nested member expressions', function() {
+      check('@a.b', 'this.a.b');
+    });
+
+    it('does not double-expand nested computed member expressions', function() {
+      check('@[a].b', 'this[a].b');
+    });
+
+    it('does not double-expand nested prototype access member expressions', function() {
+      check('@::a.b', 'this::a.b');
+    });
   });
 
   describe('changing prototype member access into normal member access', function() {
