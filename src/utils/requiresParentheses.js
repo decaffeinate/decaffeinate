7a962e162dc60f69c14352568fbe590f8175a9be
Brian Donovan <donovan@squareup.com>
2015-03-06 07:33:26 -0800

    Add a utility to determine whether an expression needs parens.

---
+++ src/utils/requiresParentheses.js
@@ -0,0 +1,29 @@
+/**
+ * Determines whether the given node, when used as part of a larger expression
+ * node, would require parentheses around it.
+ *
+ * @param {Object} node
+ * @returns {boolean}
+ */
+export default function requiresParentheses(node) {
+  switch (node.type) {
+    case 'AssignOp':
+    case 'BitAndOp':
+    case 'BitOrOp':
+    case 'BitXorOp':
+    case 'EQOp':
+    case 'GTEOp':
+    case 'GTOp':
+    case 'LTEOp':
+    case 'LTOp':
+    case 'LogicalAndOp':
+    case 'LogicalOrOp':
+    case 'NEQOp':
+    case 'PlusOp':
+    case 'SubtractOp':
+      return true;
+
+    default:
+      return false;
+  }
+}
