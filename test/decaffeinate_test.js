70ccc7bc2c40350a5d1736c2c6e7779871d45a83
Brian Donovan <donovan@squareup.com>
2015-03-10 10:54:53 -0700

    Allow writing indented template strings for expected source.

--- test/decaffeinate_test.js
+++ test/decaffeinate_test.js
@@ -1,12 +1,9 @@
 import { convert } from '../src/index';
 import { strictEqual } from 'assert';
 
+const WHITESPACE = /^\s*$/;
 
 describe('automatic conversions', function() {
-  function check(source, expected) {
-    assert.strictEqual(convert(source), expected);
-  }
-
   describe('inserting commas', function() {
     it('does not add commas after block comments', function() {
       check(
@@ -521,4 +518,49 @@ describe('automatic conversions', function() {
       check('a unless b', 'if (!b) { a; }');
     });
   });
+
+  function check(source, expected) {
+    strictEqual(convert(stripSharedIndent(source)), stripSharedIndent(expected));
+  }
+
+  /**
+   * Removes indentation shared by all lines.
+   *
+   * @param {string} source
+   * @returns {string}
+   */
+  function stripSharedIndent(source) {
+    const lines = source.split('\n');
+
+    while (lines.length > 0 && WHITESPACE.test(lines[0])) {
+      lines.shift();
+    }
+    while (lines.length > 0 && WHITESPACE.test(lines[lines.length - 1])) {
+      lines.pop();
+    }
+
+    const minimumIndent = lines.reduce((indent, line) => {
+      if (line.length === 0) {
+        return indent;
+      } else {
+        return Math.min(getIndent(line), indent);
+      }
+    }, Infinity);
+
+    return lines.map(line => line.slice(minimumIndent)).join('\n');
+  }
+
+  /**
+   * Determines the indentation in number of spaces of a line.
+   *
+   * @param {string} line
+   * @returns {number}
+   */
+  function getIndent(line) {
+    let index = 0;
+    while (line[index] === ' ') {
+      index++;
+    }
+    return index;
+  }
 });
