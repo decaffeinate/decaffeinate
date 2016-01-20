f730723780f6ca84a0ed68f2425f7d7113b3a5d6
Brian Donovan <me@brian-donovan.com>
2016-01-19 22:11:23 -0500

    Include source maps for test builds to get better stack traces.

--- rollup.test.config.js
+++ rollup.test.config.js
@@ -1,9 +1,11 @@
 import babel from 'rollup-plugin-babel';
-import multiEntry, { entry } from 'rollup-plugin-multi-entry';
+import multiEntry from 'rollup-plugin-multi-entry';
 
 export default {
-  entry,
-  plugins: [babel(), multiEntry('test/**/*_test.js')],
+  entry: 'test/**/*_test.js',
+  plugins: [babel(), multiEntry()],
   format: 'cjs',
-  dest: 'build/test-bundle.js'
+  dest: 'build/test-bundle.js',
+  sourceMap: true,
+  intro: `require('source-map-support').install();`
 };
