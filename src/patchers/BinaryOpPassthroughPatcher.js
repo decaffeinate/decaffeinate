e1c7482b57e4b7b7cb5f2b03b2edd4f5b730a6ca
Brian Donovan <me@brian-donovan.com>
2016-02-05 03:15:30 -0800

    Add types to BinaryOpPassthroughPatcher.

--- src/patchers/BinaryOpPassthroughPatcher.js
+++ src/patchers/BinaryOpPassthroughPatcher.js
@@ -1,7 +1,8 @@
 import NodePatcher from './NodePatcher';
+import type { Node, ParseContext, Editor } from './types';
 
 export default class BinaryOpPassthroughPatcher extends NodePatcher {
-  constructor(node, context, editor, left, right) {
+  constructor(node: Node, context: ParseContext, editor: Editor, left: NodePatcher, right: NodePatcher) {
     super(node, context, editor);
     this.left = left;
     this.right = right;
