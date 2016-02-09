27a65b410d7f430a5f3da93d9d6fabf7b56bfdc1
Brian Donovan <me@brian-donovan.com>
2016-02-09 14:26:42 -0800

    Improve support for `throw`.
    This ensures that `throw` used as an expression works correctly, that it communicates that it prefers not to be used that way, and that it cannot be implicitly returned.

--- src/patchers/ThrowPatcher.js
+++ src/patchers/ThrowPatcher.js
@@ -7,6 +7,49 @@ export default class ThrowPatcher extends NodePatcher {
     this.expression = expression;
   }
 
+  /**
+   * Throw in JavaScript is a statement only, so we'd prefer it stay that way.
+   */
+  prefersToPatchAsExpression(): boolean {
+    return false;
+  }
+
+  /**
+   * Throw statements that are in the implicit return position should simply
+   * be left alone as they're pure statements in JS and don't have a value.
+   */
+  setImplicitlyReturns() {
+    // throw can't be an implicit return
+  }
+
+  /**
+   * `throw` statements cannot normally be used as expressions, so we wrap them
+   * in an arrow function IIFE.
+   */
+  patchAsExpression() {
+    let hasParens = this.isSurroundedByParentheses();
+    if (!hasParens) {
+      // `throw err` → `(throw err`
+      //                ^
+      this.insertAtStart('(');
+    }
+    // `(throw err` → `(() => { throw err`
+    //                  ^^^^^^^^
+    this.insertAtStart('() => { ');
+    this.patchAsStatement();
+    // `(() => { throw err` → `(() => { throw err }`
+    //                                           ^^
+    this.insertAtEnd(' }');
+    if (!hasParens) {
+      // `(() => { throw err }` → `(() => { throw err })`
+      //                                               ^
+      this.insertAtEnd(')');
+    }
+    // `(() => { throw err })` → `(() => { throw err })()`
+    //                                                 ^^
+    this.insertAfter('()');
+  }
+
   patchAsStatement() {
     this.expression.patch();
   }
