e92ed35ba7a3d7e694ad5bdece5151a3839e6cd7
Brian Donovan <me@brian-donovan.com>
2016-02-18 10:31:27 -0800

    Allow using the passthrough patcher for nodes with children.

--- src/patchers/PassthroughPatcher.js
+++ src/patchers/PassthroughPatcher.js
@@ -1,5 +1,19 @@
 import NodePatcher from './NodePatcher';
+import type { Editor, Node, ParseContext } from './types';
 
 export default class PassthroughPatcher extends NodePatcher {
-  patch() {}
+  constructor(node: Node, context: ParseContext, editor: Editor, ...children: Array<?NodePatcher|Array<?NodePatcher>>) {
+    super(node, context, editor);
+    this.children = children;
+  }
+
+  patch() {
+    this.children.forEach(child => {
+      if (Array.isArray(child)) {
+        child.forEach(child => child && child.patch());
+      } else if (child) {
+        child.patch();
+      }
+    });
+  }
 }
