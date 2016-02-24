b8cbdfff7c7f46951a52f9e042f0e0dcbe16959c
Brian Donovan <me@brian-donovan.com>
2016-02-24 09:27:42 -0800

    WIP

--- src/patchers/LogicalOpCompoundAssignOpPatcher.js
+++ src/patchers/LogicalOpCompoundAssignOpPatcher.js
@@ -1,7 +1,7 @@
 import CompoundAssignOpPatcher from './CompoundAssignOpPatcher';
 
 export default class LogicalOpCompoundAssignOpPatcher extends CompoundAssignOpPatcher {
-  patch() {
+  patchAsExpression() {
     let operator = this.getOperatorToken();
     // `a &&= b` → `a && b`
     //      ^
@@ -14,5 +14,32 @@ export default class LogicalOpCompoundAssignOpPatcher extends CompoundAssignOpPa
     // `a && (a = b` → `a && (a = b)`
     //                             ^
     this.insert(this.expression.after, ')');
+  }
+
+  patchAsStatement() {
+    let operator = this.getOperatorToken();
+    let op = this.context.source.slice(...operator.range);
+
+    // `a ||= b` → `if (a ||= b`
+    //              ^^^^
+    this.insertBefore('if (');
+
+    if (op === '||=') {
+      this.assignee.negate();
+    }
+
+    let assigneeAgain = this.assignee.makeRepeatable(false);
+    this.assignee.patch();
+
+    // `if (a ||= b` → `if (a) { a = b`
+    //       ^^^^            ^^^^^^^
+    this.overwrite(
+      this.assignee.after,
+      operator.range[1],
+      `) { ${assigneeAgain} =`
+    );
+    // `if (a) { a = b` → `if (a) { a = b }
+    //                                   ^^
+    this.insert(this.expression.after, ' }');
   }
 }
