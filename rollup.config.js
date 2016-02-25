bdb348a903477f7ed735bc1e6900eae35d1a35cc
Brian Donovan <me@brian-donovan.com>
2016-02-25 15:49:45 -0800

    Fix the node v0.12 build.

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,15 @@
+/**
+ * NOTE: This file must only use node v0.12 features + ES modules.
+ */
+
 import babel from 'rollup-plugin-babel';
 import { readFileSync } from 'fs';
 
-let babelConfig = JSON.parse(readFileSync('.babelrc', { encoding: 'utf8' }));
+const babelConfig = JSON.parse(readFileSync('.babelrc', { encoding: 'utf8' }));
 babelConfig.babelrc = false;
-babelConfig.presets = babelConfig.presets.map(
-  preset => preset === 'es2015' ? 'es2015-rollup' : preset
-);
+babelConfig.presets = babelConfig.presets.map(function(preset) {
+  return preset === 'es2015' ? 'es2015-rollup' : preset;
+});
 
 export default {
   entry: 'src/index.js',
