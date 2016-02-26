e226ab58898f80e6ef31a775a24cd4abe0077b0a
Brian Donovan <me@brian-donovan.com>
2016-02-25 17:51:37 -0800

    Don't use `const` for old node support.

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@
 import babel from 'rollup-plugin-babel';
 import { readFileSync } from 'fs';
 
-const babelConfig = JSON.parse(readFileSync('.babelrc', { encoding: 'utf8' }));
+var babelConfig = JSON.parse(readFileSync('.babelrc', { encoding: 'utf8' }));
 babelConfig.babelrc = false;
 babelConfig.presets = babelConfig.presets.map(function(preset) {
   return preset === 'es2015' ? 'es2015-rollup' : preset;
