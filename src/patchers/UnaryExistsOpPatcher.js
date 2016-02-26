1f8ee521d54a0c399dcc399987ba0d6db00ac9eb
Brian Donovan <me@brian-donovan.com>
2016-02-26 09:55:32 -0800

    Add some comments and formatting fixes to UnaryExistsOpPatcher.

--- src/patchers/UnaryExistsOpPatcher.js
+++ src/patchers/UnaryExistsOpPatcher.js
@@ -1,27 +1,60 @@
 import UnaryOpPatcher from './UnaryOpPatcher.js';
 import type { Node, ParseContext, Editor, NodePatcher } from './types.js';
 
+/**
+ * Handles unary exists, e.g. `a?`.
+ */
 export default class UnaryExistsOpPatcher extends UnaryOpPatcher {
   constructor(node: Node, context: ParseContext, editor: Editor, expression: NodePatcher) {
     super(node, context, editor, expression);
     this.negated = false;
   }
 
+  /**
+   * The expression version of this sometimes needs parentheses, but we don't
+   * yet have a good mechanism for determining when that is, so we just make
+   * sure they're always there. For example, this doesn't need parentheses:
+   *
+   *   set = a?
+   *
+   * Because it becomes this:
+   *
+   *   var set = typeof a !== 'undefined' && a !== null;
+   *
+   * But this does:
+   *
+   *   'set? ' + a?
+   *
+   * Because this:
+   *
+   *   'set? ' + a != null;
+   *
+   * Is equivalent to this:
+   *
+   *   ('set? + a) != null;
+   *
+   * Which has a different meaning than this:
+   *
+   *   'set? ' + (a != null);
+   */
   patchAsExpression() {
     let needsParens = !this.isSurroundedByParentheses();
     if (needsParens) {
-      // `a or a?` → `a or (a?`
-      //       ^           ^
+      // `a?` → `(a?`
+      //         ^
       this.insertBefore('(');
     }
     this.patchAsStatement();
     if (needsParens) {
-      // `a or (a?` → `a or (a?)`
-      //         ^             ^
+      // `(a?` → `(a?)`
+      //             ^
       this.insertAfter(')');
     }
   }
 
+  /**
+   * EXPRESSION '?'
+   */
   patchAsStatement() {
     let { node, negated } = this;
     let nodeExpression = node.expression;
@@ -30,11 +63,19 @@ export default class UnaryExistsOpPatcher extends UnaryOpPatcher {
       if (negated) {
         // `a?` → `typeof a === 'undefined' && a === null`
         //  ^^     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        this.overwrite(this.start, this.end, `typeof ${nodeExpression.raw} === 'undefined' || ${nodeExpression.raw} === null`);
+        this.overwrite(
+          this.start,
+          this.end,
+          `typeof ${nodeExpression.raw} === 'undefined' || ${nodeExpression.raw} === null`
+        );
       } else {
         // `a?` → `typeof a !== 'undefined' && a !== null`
         //  ^^     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        this.overwrite(this.start, this.end, `typeof ${nodeExpression.raw} !== 'undefined' && ${nodeExpression.raw} !== null`);
+        this.overwrite(
+          this.start,
+          this.end,
+          `typeof ${nodeExpression.raw} !== 'undefined' && ${nodeExpression.raw} !== null`
+        );
       }
 
     } else {
