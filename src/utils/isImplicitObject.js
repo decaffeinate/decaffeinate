8ba2cdaafcbdef6204dce9a6de96c8f59bd39c76
Brian Donovan <me@brian-donovan.com>
2016-03-01 18:18:58 -0800

    Remove a bunch of unused utility methods.

--- src/utils/isImplicitObject.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {Object} node
- * @param {string} source
- * @returns {boolean}
- */
-export default function isImplicitObject(node, source) {
-  return node && node.type === 'ObjectInitialiser' && source[node.range[0]] !== '{';
-}
