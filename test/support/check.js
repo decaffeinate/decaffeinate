a26bb6b0d76c94d97737de41f7e02ed665748528
Brian Donovan <me@brian-donovan.com>
2016-03-10 16:58:12 -0800

    Print more information about PatchErrors in tests.

--- test/support/check.js
+++ test/support/check.js
@@ -1,8 +1,16 @@
+import PatchError from '../../src/utils/PatchError.js';
 import stripSharedIndent from '../../src/utils/stripSharedIndent.js';
 import { convert } from '../../dist/decaffeinate.cjs.js';
 import { strictEqual } from 'assert';
 
 export default function check(source, expected) {
-  let converted = convert(stripSharedIndent(source));
-  strictEqual(converted.code, stripSharedIndent(expected));
+  try {
+    let converted = convert(stripSharedIndent(source));
+    strictEqual(converted.code, stripSharedIndent(expected));
+  } catch (err) {
+    if (PatchError.isA(err)) {
+      console.error(PatchError.prettyPrint(err));
+    }
+    throw err;
+  }
 }
