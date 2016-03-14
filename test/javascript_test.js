8243c538723fb2a20f96cd76c030be432a18ad5b
Brian Donovan <me@brian-donovan.com>
2016-03-14 12:20:22 -0700

    Allow passing JSX through.
    Sort of related to https://github.com/esnext/esnext/pull/88.

--- test/javascript_test.js
+++ test/javascript_test.js
@@ -8,4 +8,8 @@ describe('embedded JavaScript', () => {
   it('strips the backticks off in an expression context', () => {
     check('a = `void 0`', 'let a = void 0;');
   });
+
+  it('allows passing JSX through', () => {
+    check('-> `<MyComponent />`', '() => <MyComponent />;');
+  });
 });
