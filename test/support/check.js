e85c74f5392b07b51853945c5eb0673a537ba1e9
Brian Donovan <me@brian-donovan.com>
2016-03-18 14:23:19 -0700

    Add support for unit testing and test `BlockPatcher`.

--- test/support/check.js
+++ test/support/check.js
@@ -1,5 +1,9 @@
+import MagicString from 'magic-string';
+import MainStage from '../../src/stages/main/index.js';
 import PatchError from '../../src/utils/PatchError.js';
+import parse from '../../src/utils/parse.js';
 import stripSharedIndent from '../../src/utils/stripSharedIndent.js';
+import type ProgramPatcher from '../../src/stages/main/patchers/ProgramPatcher.js';
 import { convert } from '../../dist/decaffeinate.cjs.js';
 import { strictEqual } from 'assert';
 
@@ -13,4 +17,22 @@ export default function check(source, expected) {
     }
     throw err;
   }
+}
+
+type UnitTestConfig = {
+  source: string,
+  equals: string,
+  after: (program: ProgramPatcher) => void
+};
+
+export function unit(config: UnitTestConfig) {
+  let { source, equals, after } = config;
+  source = stripSharedIndent(source);
+  equals = stripSharedIndent(equals);
+  let editor = new MagicString(source);
+  let ast = parse(source);
+  let stage = new MainStage(ast, ast.context, editor);
+  stage.build();
+  after(stage.root);
+  strictEqual(editor.toString(), equals);
 }
