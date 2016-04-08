c3ea7370a779e7d9d850b82c4a51cf3b793cb516
Brian Donovan <me@brian-donovan.com>
2016-04-08 11:22:27 -0700

    Add support for `do` expressions.

--- src/stages/main/patchers/DefaultParamPatcher.js
+++ src/stages/main/patchers/DefaultParamPatcher.js
@@ -1,3 +1,22 @@
-import BinaryOpPatcher from './BinaryOpPatcher.js';
+import NodePatcher from '../../../patchers/NodePatcher.js';
+import type { Editor, Node, ParseContext } from '../../../patchers/types.js';
 
-export default class DefaultParamPatcher extends BinaryOpPatcher {}
+export default class DefaultParamPatcher extends NodePatcher {
+  param: NodePatcher;
+  value: NodePatcher;
+  
+  constructor(node: Node, context: ParseContext, editor: Editor, param: NodePatcher, value: NodePatcher) {
+    super(node, context, editor);
+    this.param = param;
+    this.value = value;
+  }
+  
+  patchAsExpression() {
+    this.param.patch();
+    this.value.patch();
+  }
+  
+  patchAsStatement() {
+    this.patchAsExpression();
+  }
+}
