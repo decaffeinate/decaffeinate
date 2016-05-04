60572ca3f790d51dc0627026596dc34220b3f519
Brian Donovan <me@brian-donovan.com>
2016-05-04 13:17:47 -0700

    Bring `NewOpPatcher` in line with `FunctionApplicationPatcher`. (#199)
    This ensures they're always patched the same way. Fixes #198.

--- src/stages/main/patchers/NewOpPatcher.js
+++ src/stages/main/patchers/NewOpPatcher.js
@@ -1,51 +1,6 @@
-import NodePatcher from './../../../patchers/NodePatcher.js';
-import type { Node, ParseContext, Editor } from './../../../patchers/types.js';
-import { COMMA } from 'coffee-lex';
+import FunctionApplicationPatcher from './FunctionApplicationPatcher.js';
 
 /**
  * Handles construction of objects with `new`.
  */
-export default class NewOpPatcher extends NodePatcher {
-  ctor: NodePatcher;
-  args: Array<NodePatcher>;
-  
-  constructor(node: Node, context: ParseContext, editor: Editor, ctor: NodePatcher, args: Array<NodePatcher>) {
-    super(node, context, editor);
-    this.ctor = ctor;
-    this.args = args;
-  }
-
-  initialize() {
-    this.ctor.setRequiresExpression();
-    this.args.forEach(arg => arg.setRequiresExpression());
-  }
-
-  patchAsExpression() {
-    this.ctor.patch();
-    let implicitCall = this.isImplicitCall();
-    if (this.args.length > 0) {
-      if (implicitCall) {
-        this.overwrite(this.ctor.outerEnd, this.args[0].outerStart, '(');
-      }
-      this.args.forEach((arg, i, args) => {
-        arg.patch();
-        let isLast = i === args.length - 1;
-        if (!isLast && !arg.hasSourceTokenAfter(COMMA)) {
-          this.insert(arg.outerEnd, ',');
-        }
-      });
-      if (implicitCall) {
-        this.insert(this.args[this.args.length - 1].outerEnd, ')');
-      }
-    } else if (implicitCall) {
-      this.insert(this.ctor.outerEnd, '()');
-    }
-  }
-
-  /**
-   * @private
-   */
-  isImplicitCall(): boolean {
-    return this.context.source[this.ctor.outerEnd] !== '(';
-  }
-}
+export default class NewOpPatcher extends FunctionApplicationPatcher {}
