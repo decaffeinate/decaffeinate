702978a166033345a7c4def0b25c21c825742c10
Brian Donovan <me@brian-donovan.com>
2016-06-28 19:59:58 -0700

    chore: update gh-pages branch on successful builds

--- script/helpers.sh
+++ script/helpers.sh
@@ -8,3 +8,11 @@ hasChanges() {
     return 0
   fi
 }
+
+configureGithubRemote() {
+  if [ -n "$GH_TOKEN" ]; then
+    git remote set-url origin "https://$GH_TOKEN@github.com/decaffeinate/decaffeinate.git"
+    git config user.name "Brian Donovan"
+    git config user.email "me@brian-donovan.com"
+  fi
+}
--- script/update-gh-pages.sh
+++ script/update-gh-pages.sh
@@ -2,7 +2,14 @@
 
 set -e
 
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+  echo "No GitHub pages update on the $TRAVIS_BRANCH branch."
+  exit 0
+fi
+
 source $(dirname $0)/helpers.sh
+
+configureGithubRemote
 
 # Get the current version.
 VERSION=$(./script/latest-version $(node -e 'console.log(require("./package.json")["name"])'))
@@ -11,17 +18,13 @@ VERSION=$(./script/latest-version $(node -e 'console.log(require("./package.json
 browserify -e dist/decaffeinate.js -s decaffeinate -o decaffeinate.js
 
 # Switch to gh-pages branch.
-git fetch origin
-git checkout gh-pages
-git reset --hard origin/gh-pages || echo "No updates from server."
+git fetch -f origin gh-pages:gh-pages
+git reset --hard gh-pages
 
 # Update the script in the gh-pages branch.
 mv decaffeinate.js scripts/
 perl -p -i -e "s/v\d+\.\d+\.\d+/v$VERSION/" repl/index.html
 if hasChanges; then
   git commit -av -m "Update decaffeinate.js."
-  git push origin gh-pages
+  git push origin HEAD:gh-pages
 fi
-
-# Go back to the master branch.
-git checkout master
