492c944e74785ab691ac6cbfb776b5e677ef353b
Brian Donovan <me@brian-donovan.com>
2016-07-22 17:36:21 +0000

    Update decaffeinate.js.

--- scripts/decaffeinate.js
+++ scripts/decaffeinate.js
@@ -7446,6 +7446,34 @@ var TemplateLiteralPatcher = function (_NodePatcher) {
   }
 
   createClass(TemplateLiteralPatcher, [{
+    key: 'initialize',
+    value: function initialize() {
+      var _iteratorNormalCompletion = true;
+      var _didIteratorError = false;
+      var _iteratorError = undefined;
+
+      try {
+        for (var _iterator = this.expressions[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+          var expression = _step.value;
+
+          expression.setRequiresExpression();
+        }
+      } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+          }
+        } finally {
+          if (_didIteratorError) {
+            throw _iteratorError;
+          }
+        }
+      }
+    }
+  }, {
     key: 'patchAsExpression',
     value: function patchAsExpression() {
       var quasis = this.quasis;
