ae72c0e9ea83165f2e59190d306f6db64e251bdd
Alan Pierce <alangpierce@gmail.com>
2016-10-26 22:12:44 -0700

    fix: properly handle break and continue in loop expressions (#476)
    Fixes #473
    
    `break` and `continue` were parsed as regular identifiers, so the loop
    expression patching code would sometimes make a lambda trying to return them or
    generated code like `result.push(break)`. Probably the cleanest fix would be to
    parse `break` and `continue` differently in the AST, but the easy fix is to just
    say that the identifier can't be patched as an expression in those cases, and
    handle that properly.

--- src/stages/main/patchers/IdentifierPatcher.js
+++ src/stages/main/patchers/IdentifierPatcher.js
@@ -8,4 +8,12 @@ export default class IdentifierPatcher extends PassthroughPatcher {
   isRepeatable(): boolean {
     return true;
   }
+
+  /**
+   * Currently, break and continue are parsed as identifiers, but they need to
+   * behave differently in some cases.
+   */
+  canPatchAsExpression(): boolean {
+    return this.node.data !== 'break' && this.node.data !== 'continue';
+  }
 }
