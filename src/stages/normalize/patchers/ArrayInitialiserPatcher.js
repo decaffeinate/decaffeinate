28d7b207e2e20ade31b9123ce3eddcf2c3a0de70
Alan Pierce <alangpierce@gmail.com>
2016-11-05 22:08:42 -0700

    fix: remove unnecessary commas in the normalize stage (#526)
    Fixes #447.
    
    CoffeeScript is not particularly consistent about when to allow trailing commas
    in multiline argument lists, array literals, and object literals. If the last
    part of the argument or element is an implicit function call, then they are
    allowed, but if the function call has parens, then the trailing comma is not
    allowed. This means that the normalize stage can make code incorrect in some
    cases, so we need to remove these unnecessary parens.

---
+++ src/stages/normalize/patchers/ArrayInitialiserPatcher.js
@@ -0,0 +1,26 @@
+import NodePatcher from './../../../patchers/NodePatcher.js';
+import type { Editor, Node, ParseContext } from './../../../patchers/types.js';
+import { COMMA } from 'coffee-lex';
+
+export default class ArrayInitialiserPatcher extends NodePatcher {
+  members: Array<NodePatcher>;
+
+  constructor(node: Node, context: ParseContext, editor: Editor, members: Array<NodePatcher>) {
+    super(node, context, editor);
+    this.members = members;
+  }
+
+  patchAsExpression() {
+    for (let member of this.members) {
+      // If the last token of the arg is a comma, then the actual delimiter must
+      // be a newline and the comma is unnecessary and can cause a syntax error
+      // when combined with other normalize stage transformations. So just
+      // remove the redundant comma.
+      let lastToken = member.lastToken();
+      if (lastToken.type === COMMA) {
+        this.remove(lastToken.start, lastToken.end);
+      }
+      member.patch();
+    }
+  }
+}
