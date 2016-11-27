bbd2c8269d0f3bf831ae53fec5b9f8599d7bf4e3
Alan Pierce <alangpierce@gmail.com>
2016-11-27 14:38:33 -0800

    feat: add support for heregex templates
    Fixes #557
    Fixes #341
    
    Most of the existing code in StringPatcher was also relevant for heregexes, so I
    pulled the shared code into an InterpolatedPatcher class.
    
    One oddity here is the way backslashes are handled in heregexes. You can use a
    backslash to escape a whitespace character (i.e. to include it), but other than
    that, backslashes just represent the backslash character. So when turning a
    heregex into a string, we need to escape backslashes when not followed by
    whitespace, but skip them if they are. To accomplish this, I generalized the
    `escape` function to parameterize how it skips characters.
    
    With this change, decaffeinate can convert all 1213 files in my codebase!

--- test/regular_expression_test.js
+++ test/regular_expression_test.js
@@ -13,14 +13,17 @@ describe('regular expressions', () => {
     check(`a.a(/#/)`, `a.a(/#/);`);
   });
 
-  it('rewrites block regular expressions as normal regular expressions', () => {
+  it('rewrites block regular expressions as a RegExp call with a multiline string', () => {
     check(`
       a = ///
         foo .*
         bar
       ///
     `, `
-      let a = /foo.*bar/;
+      let a = new RegExp(\`\\
+      foo.*\\
+      bar\\
+      \`);
     `);
   });
 
@@ -36,7 +39,29 @@ describe('regular expressions', () => {
     check(`/\\/\\//`, `/\\/\\//;`);
   });
 
-  it('escapes slashes in heregexes', () => {
-    check(`///a/b///`, `/a\\/b/;`);
+  it('escapes necessary characters in heregexes', () => {
+    check(`///a/b\`c\${d\\e///`, `new RegExp(\`a/b\\\`c\\\${d\\\\e\`);`);
+  });
+
+  it('properly handles flags in heregexes', () => {
+    check(`///a\nb///gi`, `new RegExp(\`a\\\nb\`, 'gi');`);
+  });
+
+  it('handles interpolations within heregexes', () => {
+    check(`
+      ///
+        foo  # hello #{abc}d
+        #{bar}
+      ///g
+    `, `
+      new RegExp(\`\\
+      foo\${abc}d\\
+      \${bar}\\
+      \`, 'g');
+    `);
+  });
+
+  it('allows escaping spaces in heregexes', () => {
+    check(`///a\\ b\\\tc\\\nd///`, `new RegExp(\`a b\tc\nd\`);`);
   });
 });
