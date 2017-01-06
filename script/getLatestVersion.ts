a25a8f6559a0b8264024ff70d58a92bb8fcc2f69
Brian Donovan <me@brian-donovan.com>
2017-01-06 08:33:08 -0800

    build: add an improved update-website script (#725)

---
+++ script/getLatestVersion.ts
@@ -0,0 +1,25 @@
+import { get } from 'https';
+import { format, parse } from 'url';
+
+let registry: string = require('../package.json').publishConfig.registry;
+
+export default function getLatestVersion(packageName: string): Promise<string> {
+  let url = parse(registry);
+  url.pathname = `/${packageName}`;
+
+  return new Promise((resolve, reject) => {
+    get(format(url), response => {
+      let body = '';
+      response.setEncoding('utf8');
+      response.on('data', chunk => body += chunk);
+      response.on('end', () => {
+        if (response.statusCode !== 200) {
+          reject(new Error(`unable to get latest version (code=${response.statusCode}): ${body}`));
+        }
+
+        let packageInfo = JSON.parse(body);
+        resolve(packageInfo['dist-tags']['latest']);
+      });
+    }).on('error', reject);
+  });
+}
---
+++ script/update-website.ts
@@ -0,0 +1,121 @@
+import getLatestVersion from './getLatestVersion';
+import { execFile } from 'mz/child_process';
+import { readFile } from 'mz/fs';
+import { join } from 'path';
+
+let commit = true;
+let force = false;
+
+for (let i = 2; i < process.argv.length; i++) {
+  switch (process.argv[i]) {
+    case '--no-commit':
+      commit = false;
+      break;
+
+    case '--force':
+    case '-f':
+      force = true;
+      break;
+
+    default:
+      console.error('error: unexpected argument:', process.argv[i]);
+      console.error('update-website [--no-commit|--force]');
+  }
+}
+
+async function pkg(): Promise<{}> {
+  let content = await readFile(join(__dirname, '../package.json'), { encoding: 'utf8' });
+  return JSON.parse(content);
+}
+
+async function configureGithubRemote(name: string, project: string): Promise<void> {
+  let githubToken = process.env['GH_TOKEN'];
+  let url = githubToken ? `https://${githubToken}@github.com/${project}.git` : `https://github.com/${project}.git`;
+
+  try {
+    await run('git', ['remote', 'remove', 'website']);
+  } catch (err) {
+    // that's fine
+  }
+
+  console.log(`Adding remote ${name}…`);
+  await run('git', ['remote', 'add', '-f', name, url]);
+  await run('git', ['config', 'user.name', 'Brian Donovan']);
+  await run('git', ['config', 'user.email', 'me@brian-donovan.com']);
+}
+
+async function run(command: string, args: Array<string>): Promise<{ stdout: string, stderr: string }> {
+  let [ stdout, stderr ] = await execFile(command, args);
+  return { stdout, stderr };
+}
+
+async function gitRevParse(ref: string): Promise<string> {
+  let { stdout } = await run('git', ['rev-parse', ref]);
+  return stdout.trim();
+}
+
+async function hasChanges(): Promise<boolean> {
+  // update the cache
+  await run('git', ['status']);
+
+  try {
+    await run('git', ['diff-index', '--exit-code', 'HEAD', '--']);
+    return false;
+  } catch (err) {
+    return true;
+  }
+}
+
+async function updateWebsite() {
+  await configureGithubRemote('website', 'decaffeinate/decaffeinate-project.org');
+
+  let latestVersion = await getLatestVersion((await pkg())['name']);
+  let currentRef = await gitRevParse('HEAD');
+  let decaffeinatePackage = await pkg();
+  let decaffeinateRegistry = decaffeinatePackage['publishConfig'];
+
+  console.log('Setting up website repo…');
+  await run('git', ['fetch', '-f', 'website', 'master:website-master']);
+  await run('git', ['reset', '--hard', 'website-master']);
+
+  let websitePackage = await pkg();
+  let currentVersion = websitePackage['devDependencies']['decaffeinate'];
+
+  if (Math.random() < 1 || currentVersion === latestVersion) {
+    console.log(`Already using decaffeinate v${latestVersion}, skipping install.`);
+  } else {
+    console.log(`${currentVersion} != ${latestVersion}, installing decaffeinate v${latestVersion}…`);
+    let args = [
+      'install',
+      '--save-dev',
+      '--save-exact',
+      `decaffeinate@${latestVersion}`
+    ];
+
+    if (decaffeinateRegistry) {
+      args.unshift('--registry', decaffeinateRegistry);
+    }
+
+    await run('npm', args);
+
+    if (await hasChanges()) {
+      if (commit) {
+        console.log('Pushing changes to website repo…');
+        await run('git', ['commit', '-av', '-m', `chore: update to decaffeinate ${latestVersion}`]);
+        await run('git', ['push', 'website', 'HEAD:master']);
+      } else {
+        console.log('Not committing because --no-commit was given:');
+        console.log((await run('git', ['diff'])).stdout);
+      }
+    }
+  }
+
+  console.log(`Switching back to ${currentRef.slice(0, 7)}…`);
+  await run('git', ['reset', '--hard', currentRef]);
+}
+
+updateWebsite()
+  .catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+  });
