decaffeinate/decaffeinate

commit 7ad17f8350e6aa1fb5b12cfff18974b2fdf7a826

fix: do not insert code outside array elements (#719)
Ensure that array elements are considered bounding patchers. This prevents, for example, appending trailing `}` for functions outside the array.

Closes #716

src/patchers/NodePatcher.js
@@ -888,6 +888,8 @@ export default class NodePatcher {
       if (this.isNodeFunctionApplication(this.parent.node) &&
           this.parent.node.arguments.some(arg => arg === this.node)) {
         return this;
+      } else if (this.parent.node.type === 'ArrayInitialiser') {
+        return this;
       }
       return this.parent.getBoundingPatcher();
     } else {

test/array_test.js
@@ -120,4 +120,17 @@ describe('arrays', () => {
       }))];
     `);
   });
+
+  it('keeps array elements inserting within the array', () =>
+    check(`
+      [->
+        a
+        b]
+    `, `
+      [function() {
+        a;
+        return b;
+      }];
+    `)
+  );
 });

commit 1d5cae6cbf33be48209a5b3ca2c3af98bc0260b7

fix: make edit bounds checking within an array more lenient
Fixes #740

I think fixes a regression from #719.

The assertion that we edit with bounds should be a little more lenient than the
code that limits writing to the bounding patcher. We already had a case for that
for function parameters, so add one for array elements as well.

The resulting code in the test is a bit ugly, but at least it doesn't crash.

src/patchers/NodePatcher.js
@@ -415,7 +415,8 @@ export default class NodePatcher {
     // comma/paren to the next comma/paren), so loosen the restriction to the
     // entire function.
     if (boundingPatcher.parent &&
-        this.isNodeFunctionApplication(boundingPatcher.parent.node)) {
+        (this.isNodeFunctionApplication(boundingPatcher.parent.node) ||
+        boundingPatcher.parent.node.type === 'ArrayInitialiser')) {
       boundingPatcher = boundingPatcher.parent;
     }
     if (this.allowPatchingOuterBounds()) {

test/object_test.js
@@ -360,4 +360,19 @@ describe('objects', () => {
       ({x() { return x; }});
     `);
   });
+
+  it('handles an implicit object inside an array', () => {
+    check(`
+      [
+        a: b
+        c: d
+      ]
+    `, `
+      [{
+        a: b,
+        c: d
+      }
+      ];
+    `);
+  });
 });