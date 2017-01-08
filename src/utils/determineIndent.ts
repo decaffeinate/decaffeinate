decaffeinate/decaffeinate

commit 739f40dc0dd1b9d2f55f1e9d59d72b7470afc7c8

refactor: convert a few utilities and tests to TypeScript (#735)

src/utils/determineIndent.ts
@@ -1,11 +1,9 @@
-/* @flow */
-
-import detectIndent from 'detect-indent';
+import detectIndent = require('detect-indent');
 
 const DEFAULT_INDENT = '  ';
 
 export default function determineIndent(source: string): string {
-  let indent = detectIndent(source);
+  let indent: { amount: number, type: 'space' | 'tab', indent: string } = detectIndent(source);
   if (indent.type === 'space' && indent.amount % 2 === 1) {
     return DEFAULT_INDENT;
   }

rollup.config.js
@@ -5,6 +5,8 @@
 import babel from 'rollup-plugin-babel';
 import json from 'rollup-plugin-json';
 import babelrc from 'babelrc-rollup';
+import ts from 'rollup-plugin-typescript';
+import * as TypeScript from 'typescript';
 
 var pkg = require('./package.json');
 var external = Object.keys(pkg.dependencies).concat(['path', 'fs']);
@@ -13,6 +15,7 @@ export default {
   entry: 'src/index.js',
   plugins: [
     json(),
+    ts({ typescript: TypeScript }),
     babel(babelrc())
   ],
   external: external,

script/getLatestVersion.ts
@@ -1,6 +1,7 @@
 import { get } from 'https';
 import { format, parse } from 'url';
 
+// tslint:disable:no-var-requires
 let registry: string = require('../package.json').publishConfig.registry;
 
 export default function getLatestVersion(packageName: string): Promise<string> {

script/update-website.ts
@@ -1,7 +1,7 @@
-import getLatestVersion from './getLatestVersion';
 import { execFile } from 'mz/child_process';
 import { readFile } from 'mz/fs';
 import { join } from 'path';
+import getLatestVersion from './getLatestVersion';
 
 let commit = true;
 let force = false;

src/stages/normalize/patchers/BlockPatcher.js
@@ -1,5 +1,5 @@
 import NodePatcher from './../../../patchers/NodePatcher';
-import getStartOfLine from './../../../utils/getStartOfLine';
+import getStartOfLine from '../../../utils/getStartOfLine';
 
 import type { PatcherContext } from './../../../patchers/types';
 

src/utils/adjustIndent.js.flow
@@ -1,0 +1,5 @@
+/* @flow */
+
+declare function adjustIndent(source: string, offset: number, adjustment: number): string;
+
+export default adjustIndent;

src/utils/adjustIndent.ts
@@ -1,5 +1,3 @@
-/* @flow */
-
 import determineIndent from './determineIndent';
 import getIndent from './getIndent';
 

src/utils/array/find.js.flow
@@ -1,0 +1,5 @@
+/* @flow */
+
+declare function find<T>(array: Array<T>, iterator: (element: T, i: number, array: Array<T>) => boolean, context: any): T | typeof undefined;
+
+export default find;

commit a177aff932bfc50b68bbe668b0dd8e6631ad78a1

fix: un-break CLI by changing detect-indent import to a form recognized by rollup
This fixes a consistent crash caused by #735 where any non-trivial file would
fail due to `detectIndent` not being defined. From what I can tell, rollup can't
handle the `import detectIndent = require('detect-indent');` line and was just
ignoring it, so `detect-indent` was never pulled in and everything broke when it
tried to use that method.

A little ironic that this would have been caught by tests were it not for #712.
But probably it's still fine to leave the tests as testing the source code and
not the dist build.

src/utils/determineIndent.ts
@@ -1,4 +1,4 @@
-import detectIndent = require('detect-indent');
+const detectIndent = require('detect-indent');
 
 const DEFAULT_INDENT = '  ';
 