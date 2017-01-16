c772488b555c077f8663ba40da432ce5b97e6c0e
Alan Pierce <alangpierce@gmail.com>
2017-01-16 13:46:41 -0800

    fix: wrap yield expressions in parens when necessary
    Fixes #776
    
    For many expression cases, JavaScript requires parens around yield expressions,
    so we need to add them unless we know it's safe not to. yield in a statement
    context, yield in a return, and yield on the RHS of an assignment all do not
    require parens, so we skip the parens in those cases.

--- src/stages/main/patchers/YieldFromPatcher.js
+++ src/stages/main/patchers/YieldFromPatcher.js
@@ -1,26 +1,12 @@
-import NodePatcher from './../../../patchers/NodePatcher';
-import type { PatcherContext } from './../../../patchers/types';
+import YieldPatcher from './YieldPatcher';
 
-export default class YieldFromPatcher extends NodePatcher {
-  expression: NodePatcher;
-  
-  constructor(patcherContext: PatcherContext, expression: NodePatcher) {
-    super(patcherContext);
-    this.expression = expression;
-  }
-  
-  initialize() {
-    this.yields();
-    this.expression.setRequiresExpression();
-  }
-
+export default class YieldFromPatcher extends YieldPatcher {
   /**
    * 'yield' 'from' EXPRESSION
    */
   patchAsExpression({ needsParens=true }={}) {
     let src = this.sourceTokenAtIndex(this.contentStartTokenIndex);
     this.overwrite(src.start, src.end, 'yield*');
-    
-    this.expression.patch({ needsParens });
+    super.patchAsExpression({ needsParens });
   }
 }
