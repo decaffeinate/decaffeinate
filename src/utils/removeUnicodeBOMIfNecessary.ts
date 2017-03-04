de5c9acbcb184abba2e36d8cb1bdf82d866fac36
Alan Pierce <alangpierce@gmail.com>
2017-03-03 19:35:10 -0800

    fix: remove the Unicode BOM if necessary at the start of the code
    Fixes #891
    
    Unicode byte order mark characters normally aren't a problem for us, except that
    CoffeeScript has a special case to remove them at the start, so it gives all
    source indexes as if the BOM was removed. To avoid running into issues there, we
    can just do the same. As with newline formats, this is just handled at the
    decaffeinate layer and coffee-lex and decaffeinate-parser can assume the simpler
    format (although they could also be made more robust later if it's useful).

---
+++ src/utils/removeUnicodeBOMIfNecessary.ts
@@ -0,0 +1,12 @@
+/**
+ * If the source code starts with a Unicode BOM, CoffeeScript will just ignore
+ * it and provide source code locations that assume that it was removed, so we
+ * should do the same.
+ */
+export default function removeUnicodeBOMIfNecessary(source: string): string {
+    if (source[0] === '\uFEFF') {
+        return source.slice(1);
+    } else {
+        return source;
+    }
+}
