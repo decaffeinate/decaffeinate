20a6a2f3728b55eda70d85569a76c483817d84e3
Alan Pierce <alangpierce@gmail.com>
2017-04-01 15:09:05 -0700

    fix: wrap postfix statements in parens when followed by a semicolon
    Fixes #748
    
    `a if b; c` is a conditional followed by a statement, but `if b then a; c` has
    `a; c` as the conditional body. To avoid this, we can always wrap these types of
    statements in parens if there's a semicolon after them.

--- src/utils/postfixNodeNeedsOuterParens.js
+++ src/utils/postfixNodeNeedsOuterParens.js
@@ -7,12 +7,14 @@ import type NodePatcher from '../patchers/NodePatcher';
  * Determine if the given postfix if/while/for needs to have parens wrapped
  * around it while it is reordered. This happens when the expression has a comma
  * after it as part of a list (function args, array initializer, or object
- * initializer).
+ * initializer). It also happens when there is a semicolong immediately
+ * afterward, since without the parens the next statement would be pulled into
+ * the block.
  */
 export default function postfixNodeNeedsOuterParens(patcher: NodePatcher): boolean {
   let nextToken = patcher.nextToken();
   if (nextToken) {
-    return nextToken.type === SourceType.COMMA;
+    return nextToken.type === SourceType.COMMA || nextToken.type === SourceType.SEMICOLON;
   }
   return false;
 }
