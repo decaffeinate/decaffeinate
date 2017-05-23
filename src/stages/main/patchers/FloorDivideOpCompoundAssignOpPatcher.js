085316005c782dfbb252f35796c026c62ecb5c20
Alan Pierce <alangpierce@gmail.com>
2017-05-22 20:12:43 -0700

    feat: implement compound floor division assignment
    Fixes #1029

---
+++ src/stages/main/patchers/FloorDivideOpCompoundAssignOpPatcher.js
@@ -0,0 +1,33 @@
+import CompoundAssignOpPatcher from './CompoundAssignOpPatcher';
+
+export default class FloorDivideOpCompoundAssignOpPatcher extends CompoundAssignOpPatcher {
+  patchAsExpression({ needsParens=false }={}) {
+    let shouldAddParens = this.negated ||
+      (needsParens && !this.isSurroundedByParentheses());
+    if (this.negated) {
+      this.insert(this.contentStart, '!');
+    }
+    if (shouldAddParens) {
+      this.insert(this.contentStart, '(');
+    }
+    let assigneeAgain = this.assignee.patchRepeatable({ isForAssignment: true });
+
+    // `a //= b` → `a = Math.floor(a / b`
+    //               ^^^^^^^^^^^^^^^^^
+    this.overwrite(this.assignee.outerEnd, this.expression.outerStart, ` = Math.floor(${assigneeAgain} / `);
+
+    this.expression.patch({ needsParens: true });
+
+    // `a = Math.floor(a / b` → `a = Math.floor(a / b)`
+    //                                               ^
+    this.insert(this.expression.outerEnd, ')');
+
+    if (shouldAddParens) {
+      this.insert(this.contentEnd, ')');
+    }
+  }
+
+  patchAsStatement() {
+    this.patchAsExpression();
+  }
+}
