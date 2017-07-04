9eed05193aeb58213d97e3e6497f2b1483d9cda6
Alan Pierce <alangpierce@gmail.com>
2017-07-03 21:57:34 -0700

    feat: insert suggestion comments in generated code
    Progress toward #1107
    
    Rather than printing suggestions out to the console, I think it actually makes
    more sense to include them in the code. This should make them really hard to
    miss and be especially useful for the use case of doing a large conversion and
    then going through each file as a follow-up task. I added a `disableSuggestions`
    flag to disable this behavior if people want.
    
    For now, the `disableSuggestions` is always set to true from the CLI, meaning
    the feature can't be accessed normally now; it's just exposed to tests. I'll
    change it to false by default soon along with all of the other breaking changes.

--- test/support/check.js
+++ test/support/check.js
@@ -1,9 +1,9 @@
 import PatchError from '../../src/utils/PatchError';
 import stripSharedIndent from '../../src/utils/stripSharedIndent';
 import { convert } from '../../src/index';
-import { deepEqual, strictEqual } from 'assert';
+import { strictEqual } from 'assert';
 
-export default function check(source, expected, {options={}, expectedSuggestions}={}) {
+export default function check(source, expected, {options={}}={}) {
   if (source[0] === '\n') { source = stripSharedIndent(source); }
   if (expected[0] === '\n') { expected = stripSharedIndent(expected); }
 
@@ -12,11 +12,9 @@ export default function check(source, expected, {options={}, expectedSuggestions
       preferConst: true,
       keepCommonJS: true,
       enableBabelConstructorWorkaround: true,
+      disableSuggestions: true,
     }, options));
     strictEqual(converted.code, expected);
-    if (expectedSuggestions) {
-      deepEqual(converted.suggestions, expectedSuggestions);
-    }
   } catch (err) {
     if (PatchError.detect(err)) {
       console.error(PatchError.prettyPrint(err));
