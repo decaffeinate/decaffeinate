b78340d5901feb2008b3a25bd7db641cb03801b6
Alan Pierce <alangpierce@gmail.com>
2017-10-21 16:46:12 -0700

    fix: remove parens surrounding inline JS expressions
    Fixes #1176
    
    It looks like CoffeeScript handles embedded JS comments by removing all parens
    around inline JS, even if it causes unintuitive behavior, so we copy that
    behavior.

--- src/stages/main/patchers/JavaScriptPatcher.ts
+++ src/stages/main/patchers/JavaScriptPatcher.ts
@@ -1,4 +1,5 @@
 import NodePatcher from './../../../patchers/NodePatcher';
+import FunctionApplicationPatcher from './FunctionApplicationPatcher';
 
 /**
  * Handles embedded JavaScript.
@@ -8,11 +9,23 @@ export default class JavaScriptPatcher extends NodePatcher {
    * All we have to do is strip off the backticks.
    */
   patchAsExpression(): void {
+    // For any normal surrounding parens, CoffeeScript's rule is to remove them,
+    // which allows things like comment-only inline JS. Function call parens
+    // should not be removed, though.
+    let shouldRemoveSurroundingParens =
+      !(this.parent instanceof FunctionApplicationPatcher && this === this.parent.args[0]);
+
+    if (shouldRemoveSurroundingParens) {
+      this.remove(this.outerStart, this.contentStart);
+    }
     // '`void 0`' → 'void 0`'
     //  ^
     this.remove(this.contentStart, this.contentStart + '`'.length);
     // 'void 0`' → 'void 0'
     //        ^
     this.remove(this.contentEnd - '`'.length, this.contentEnd);
+    if (shouldRemoveSurroundingParens) {
+      this.remove(this.contentEnd, this.outerEnd);
+    }
   }
 }
