0bcfbe5e530310afe2fcc2566256d9320e4c6ba1
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2018-12-08 16:12:08 -0800

    fix(package): update coffee-lex to fix a shorthand this issue
    decaffeinate/coffee-lex#175

--- test/shorthand_this_test.ts
+++ test/shorthand_this_test.ts
@@ -36,4 +36,17 @@ describe('changing shorthand this to longhand this', () => {
   it('does not double-expand nested prototype access member expressions', () => {
     check(`@::a.b`, `this.prototype.a.b;`);
   });
+
+  it('does not combine with a keyword on the next line (coffee-lex#175)', () => {
+    check(
+      `
+      a = @
+      if b then c
+      `,
+      `
+      const a = this;
+      if (b) { c; }
+      `
+    );
+  });
 });
