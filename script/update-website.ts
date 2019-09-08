a7921ed2c9b6c8d29c15d5c14d43005bcf62e210
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2019-09-08 08:50:54 -0700

    build(website): use ssh auth if no GITHUB_TOKEN

--- script/update-website.ts
+++ script/update-website.ts
@@ -25,7 +25,7 @@ async function pkg(): Promise<{}> {
 
 async function configureGithubRemote(name: string, project: string): Promise<void> {
   const githubToken = process.env['GH_TOKEN'] || process.env['GITHUB_TOKEN'];
-  const url = githubToken ? `https://${githubToken}@github.com/${project}.git` : `https://github.com/${project}.git`;
+  const url = githubToken ? `https://${githubToken}@github.com/${project}.git` : `git@github.com:${project}.git`;
 
   try {
     await run('git', ['remote', 'remove', 'website']);
